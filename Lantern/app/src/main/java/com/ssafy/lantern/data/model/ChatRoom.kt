d99b8f55340592f176c36f420969de4f76f53c5d
박수민 <psm5927@naver.com>
2025-05-02 12:36:43 +0900

    :sparkles: [Feat] room 엔티티 생성 S12P31E204-75

---
+++ Lantern/app/src/main/java/com/ssafy/lantern/data/model/ChatRoom.kt
@@ -0,0 +1,16 @@
+package com.ssafy.lantern.data.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.time.LocalDateTime
+
+@Entity(tableName = "chat_room")
+data class ChatRoom(
+    @PrimaryKey
+    @ColumnInfo(name = "chat_room_id")
+    val chatRoomId: Long,
+
+    @ColumnInfo(name = "updated_at")
+    val updatedAt: LocalDateTime
+)
--- Lantern/app/src/main/java/com/ssafy/lantern/data/model/User.kt
+++ Lantern/app/src/main/java/com/ssafy/lantern/data/model/User.kt
@@ -1,3 +1,22 @@
 package com.ssafy.lantern.data.model
 
-data class User()
+import android.provider.ContactsContract
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "user")
+data class User(
+    @PrimaryKey
+    @ColumnInfo(name = "user_id")
+    val userId: Int,
+
+    val nickname: String,
+
+    val email: String,
+
+    val password: String,
+
+    @ColumnInfo(name = "device_id")
+    val deviceId: String
+)
