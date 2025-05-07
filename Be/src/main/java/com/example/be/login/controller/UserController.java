c2a31ed25a73c58ca7fde83ad585ecc0f5e48cf0
천세윤 <tpdbs0701@gmail.com>
2025-05-07 20:50:26 +0900

    Chore : 컨트롤러 수정

--- Be/src/main/java/com/example/be/login/controller/UserController.java
+++ Be/src/main/java/com/example/be/login/controller/UserController.java
@@ -7,11 +7,9 @@ import com.example.be.login.service.GoogleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class UserController {
